96a181f2003b2ea59211c4e3088e177b0114e523
Vinci4600 <vincent.diergardt@gmail.com>
2026-05-20 14:07:12 +0200

    Add Todo_Controller

---
+++ backend/src/main/java/com/example/demo/Controller/Todo_Controller.java
@@ -0,0 +1,21 @@
+package com.example.demo.Controller;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+@RestController
+@SpringBootApplication
+@CrossOrigin
+public class Todo_Controller {
+
+}
