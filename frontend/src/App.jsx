696f5df2962695aa3a13f9956a04e57763910dc7
Vinci4600 <vincent.diergardt@gmail.com>
2026-06-03 15:17:33 +0200

    Test Pull Request

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react'
 
-import logo from './assets/react.jpg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*import logo from './assets/react.jpg'
+import viteLogo from '/vite.svg'*/
+//import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
   const [todos, setTodos] = useState([]);
   const [taskdescription, setTaskdescription] = useState("");
 
@@ -14,7 +14,7 @@ function App() {
   */
   const handleSubmit = event => {
     event.preventDefault();
-    console.log("Sending task description to Spring-Server: "+taskdescription);
+    console.log("Sending task description to Spring-Server: " + taskdescription);
     fetch("http://localhost:8080/tasks", {  // API endpoint (the complete URL!) to save a taskdescription
       method: "POST",
       headers: {
@@ -22,22 +22,22 @@ function App() {
       },
       body: JSON.stringify({ taskdescription: taskdescription }) // both 'taskdescription' are identical to Task-Class attribute in Spring
     })
-    .then(response => {
-      console.log("Receiving answer after sending to Spring-Server: ");
-      console.log(response);
-      window.location.href = "/";
-      setTaskdescription("");             // clear input field, preparing it for the next input
-    })
-    .catch(error => console.log(error))
+      .then(response => {
+        console.log("Receiving answer after sending to Spring-Server: ");
+        console.log(response);
+        window.location.href = "/";
+        setTaskdescription("");             // clear input field, preparing it for the next input
+      })
+      .catch(error => console.log(error))
   }
 
-   /** Is called when ever the html input field value below changes to update the component's state.
-  ** This is, because the submit should not take the field value directly.
-  ** The task property in the state is used to store the current value of the input field as the user types into it.
-  ** This is necessary because React operates on the principle of state and props, which means that a component's state
-  ** determines the component's behavior and render.
-  ** If we used the value directly from the HTML form field, we wouldn't be able to update the component's state and react to changes in the input field.
-  */
+  /** Is called when ever the html input field value below changes to update the component's state.
+ ** This is, because the submit should not take the field value directly.
+ ** The task property in the state is used to store the current value of the input field as the user types into it.
+ ** This is necessary because React operates on the principle of state and props, which means that a component's state
+ ** determines the component's behavior and render.
+ ** If we used the value directly from the HTML form field, we wouldn't be able to update the component's state and react to changes in the input field.
+ */
   const handleChange = event => {
     setTaskdescription(event.target.value);
   }
@@ -53,11 +53,11 @@ function App() {
   }, []);
 
 
- /** Is called when the Done-Button is pressed. It sends a POST request to the API endpoint '/delete' and updates the component's state with the new todo.
-  ** In this case if the task with the unique taskdescription is found on the server, it will be removed from the list.
-  */
+  /** Is called when the Done-Button is pressed. It sends a POST request to the API endpoint '/delete' and updates the component's state with the new todo.
+   ** In this case if the task with the unique taskdescription is found on the server, it will be removed from the list.
+   */
   const handleDelete = (event, taskdescription) => {
-    console.log("Sending task description to delete on Spring-Server: "+taskdescription);
+    console.log("Sending task description to delete on Spring-Server: " + taskdescription);
     fetch(`http://localhost:8080/delete`, { // API endpoint (the complete URL!) to delete an existing taskdescription in the list
       method: "POST",
       body: JSON.stringify({ taskdescription: taskdescription }),
@@ -65,12 +65,12 @@ function App() {
         "Content-Type": "application/json"
       }
     })
-    .then(response => {
-      console.log("Receiving answer after deleting on Spring-Server: ");
-      console.log(response);
-      window.location.href = "/";
-    })
-    .catch(error => console.log(error))
+      .then(response => {
+        console.log("Receiving answer after deleting on Spring-Server: ");
+        console.log(response);
+        window.location.href = "/";
+      })
+      .catch(error => console.log(error))
   }
 
   /**
@@ -83,8 +83,8 @@ function App() {
       <ul className="todo-list">
         {todos.map((todo, index) => (
           <li key={todo.taskdescription}>
-            <span>{"Task " + (index+1) + ": "+ todo.taskdescription}</span>
-            <button onClick={(event) => handleDelete(event, todo.taskdescription) }>&#10004;</button>
+            <span>{"Task " + (index + 1) + ": " + todo.taskdescription}</span>
+            <button onClick={(event) => handleDelete(event, todo.taskdescription)}>&#10004;</button>
           </li>
         ))}
       </ul>
@@ -92,29 +92,32 @@ function App() {
   }
 
   return (
-      <div className="div">
-    <div className="App">
-      <header className="App-header">
+    <div className="div">
+      <div className="App">
+        <header className="App-header">
 
-        <h1 className="todo">
-          ToDo Liste
-        </h1>
-        <form onSubmit={handleSubmit} className='todo-form'>
-          <label htmlFor="taskdescription">Neues Todo anlegen: </label>
+          <marquee><h1 className="todo">
+            ToDo Liste
+          </h1></marquee>
+          <form onSubmit={handleSubmit} className='todo-form'>
+            <label htmlFor="taskdescription">
+              Neue Aufgabe hinzufügen
+            </label>
 
-          <input
-            type="text"
-            value={taskdescription}
-            onChange={handleChange}
-          />
-          <button type="submit">Absenden</button>
-        </form>
-        <div>
-          {renderTasks(todos)}
-        </div>
-      </header>
-    </div>
+            <input
+              id="taskdescription"
+              type="text"
+              value={taskdescription}
+              onChange={handleChange}
+            />
+            <button type="submit">Hinzufügen</button>
+          </form>
+          <div>
+            {renderTasks(todos)}
+          </div>
+        </header>
       </div>
+    </div>
   );
 }
 
