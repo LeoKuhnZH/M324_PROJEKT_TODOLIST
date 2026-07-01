5a48266fbc415569f6b347e598cac44275b7d0c2
Vinci4600 <vincent.diergardt@gmail.com>
2026-07-01 15:03:41 +0200

    config for tests

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
   testEnvironment: 'jsdom',
-
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/src/__mocks__/fileMock.js',
   },
 };
